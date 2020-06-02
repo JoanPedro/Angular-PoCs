05723f62901fbcaa611bebca90e23ea2ff893797
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-06-01 23:30:46 -0300

    Finaliza PoC Card

---
+++ cards/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/img/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree toda',
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/img/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain',
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/img/biking.jpeg',
+      username: 'biking1222',
+      content: 'I did some biking today',
+    }
+  ]
+}
