9b56b008aa93fde379b71b4b8dbac654f9c6855a
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-06-02 14:59:23 -0300

    Finaliza PoC Pipes

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name: string;
+  date: string;
+  amount: number;
+  height: number;
+  kilometer: number;
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
+  }
+
+  onKmChange(value: string) {
+    this.kilometer = parseFloat(value);
+  }
+}
