59b72b829c5757cacb16f6241792b26c8ea3470f
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-06-03 12:49:04 -0300

    Inicia Page Navigation

---
+++ pages/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  currentPage = 0;
+
+  images = [
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1450128910652-d698afa60ac8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'At the Forest',
+      url: 'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'At the City',
+      url: 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'At the Snowy',
+      url: 'https://images.unsplash.com/photo-1461599974261-7a1559ac5267?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    }
+  ]
+}
