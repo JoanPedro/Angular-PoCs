f68378fb282e04cb2c79d53f3db816c5f38c3c67
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-06-07 11:59:39 -0300

    Diretiva ng-content

--- comps/src/app/elements/elements.module.ts
+++ comps/src/app/elements/elements.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -11,7 +12,8 @@ import { TimesDirective } from './times.directive';
   declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
   imports: [
     CommonModule,
-    ElementsRoutingModule
+    ElementsRoutingModule,
+    SharedModule,
   ],
   exports: [],
 })
---
+++ comps/src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule, enableProdMode } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DividerComponent } from './divider/divider.component';
+
+
+
+@NgModule({
+  declarations: [DividerComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    DividerComponent,
+  ]
+})
+export class SharedModule { }
