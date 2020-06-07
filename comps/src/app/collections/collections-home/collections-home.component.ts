61758550f3322641db591ab97f3e7474ffd11b25
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-06-07 18:14:16 -0300

    Finaliza Roteamento de Componentes Avançado

--- comps/src/app/collections/collections-home/collections-home.component.ts
+++ comps/src/app/collections/collections-home/collections-home.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
 
+  data = [
+    { name: 'James', age: 24, job: 'Designer', employed: 'true' },
+    { name: 'Jill', age: 31, job: 'Engineer', employed: 'false' },
+    { name: 'Christinna', age: 26, job: 'Engineer', employed: 'false' },
+  ];
+
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+    { key: 'employed', label: 'Has a job?' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
