2332e261e03449bb981a31ab8626881337d7ba64
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-06-11 12:11:52 -0300

    Finaliza Modal

--- comps/src/app/mods/modal/modal.component.ts
+++ comps/src/app/mods/modal/modal.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, OnDestroy, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
+
+  @Output() close = new EventEmitter;
 
   constructor(private el: ElementRef ) {}
 
   ngOnInit(): void {
-    document.body.appendChild(this.el.nativeElement)
+    document.body.appendChild(this.el.nativeElement);
   }
 
+  ngOnDestroy(): void {
+    this.el.nativeElement.remove();
+  }
+
+  onCloseClick(): void {
+    this.close.emit();
+  }
 }
--- comps/src/app/mods/mods.module.ts
+++ comps/src/app/mods/mods.module.ts
@@ -5,10 +5,11 @@ import { CommonModule } from '@angular/common';
 import { ModsRoutingModule } from './mods-routing.module';
 import { ModsHomeComponent } from './mods-home/mods-home.component';
 import { ModalComponent } from './modal/modal.component';
+import { AccordionComponent } from './accordion/accordion.component';
 
 
 @NgModule({
-  declarations: [ModsHomeComponent, ModalComponent],
+  declarations: [ModsHomeComponent, ModalComponent, AccordionComponent],
   imports: [
     CommonModule,
     ModsRoutingModule,
