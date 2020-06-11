ddc57008acb8efebcab0e6cf83fb6a6f63fb35c3
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-06-11 12:48:54 -0300

    Finaliza Projeto

--- comps/src/app/mods/accordion/accordion.component.ts
+++ comps/src/app/mods/accordion/accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccordionComponent implements OnInit {
 
+  @Input() items = [];
+  openedItemIndex = 0;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClick(index: number) {
+    this.openedItemIndex = index;
+  }
 }
--- comps/src/app/mods/mods-home/mods-home.component.ts
+++ comps/src/app/mods/mods-home/mods-home.component.ts
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class ModsHomeComponent implements OnInit {
 
   modalOpen: boolean = false;
+  items = [
+    { title: 'Why is the sky blue?', content: 'Sky is blue because it is.' },
+    { title: 'What does orange taste like?', content: 'An oranges tastes like an orange.' },
+    { title: 'What collor is that cat?', content: 'The cat is orange collor.' },
+  ];
 
   constructor() { }
 
