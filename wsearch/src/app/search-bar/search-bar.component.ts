f9d1f3555a0d9f6f5385137a9401af134b4f15b6
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-06-14 11:02:25 -0300

    Wikipedia Search

---
+++ wsearch/src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+
+  term: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onInput(value: string) {
+    this.term = value;
+  }
+
+  onFormSubmit() {
+    console.log(this.term);
+  }
+}
