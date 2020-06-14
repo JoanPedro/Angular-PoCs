b0be4b42c2ed870aaafd4b8841833d6327fb2441
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-06-13 21:17:21 -0300

    Typescript

--- ts/index.ts
+++ ts/index.ts
@@ -1,2 +1,25 @@
-const myName: string = 'Joan Pedro';
-console.log(myName)
+class Car {
+
+  constructor(public color: string, private year: number) {
+
+  }
+
+  drive() {
+    this.putInGear();
+    this.pressPedal();
+    this.turnWheel();
+  }
+
+  private putInGear() {
+
+  }
+
+  private pressPedal() {
+
+  }
+
+  private turnWheel() {
+
+  }
+
+}
