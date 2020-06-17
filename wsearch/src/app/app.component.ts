2abeb63241546e4ef1902b348bad86af5a15eb9e
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-06-17 08:42:21 -0300

    Adiciona callback subscribe para o service

--- wsearch/src/app/app.component.ts
+++ wsearch/src/app/app.component.ts
@@ -12,8 +12,10 @@ export class AppComponent {
   }
 
   onTerm(term: string) {
-    const results = this.wikipedia.search(term);
-    console.log(results)
+    this.wikipedia.search(term)
+      .subscribe(response => {
+        console.log(response)
+      })
   }
 
 }
